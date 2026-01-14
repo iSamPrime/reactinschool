005def5e3f2660166e6942b98210154e09d1535b
iSamPrime <isssamawad3@gmail.com>
2026-01-14 09:35:14 +0100

    idk

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-function App(){
+
+function Header(){
     return(
-        <h2>React</h2>
+        <header>
+            <h1>SAM</h1>
+            <p>Banana</p>
+        </header>
     )
+}
+
+function App(){
+    return(<>
+        <Header></Header>
+        <h2>React</h2>
+        <p>GG</p>
+
+    </>)
 }
 
 ReactDOM.createBoot(document.querySelector("#root")).render(App())
